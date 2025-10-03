35fccff49b185ea6cfed238acf99bb97c30b4941
Divya <divyayewale7175@gmail.com>
2025-10-03 12:44:57 +0530

    successfully added..when we click on login btn it will redirect to home page..

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 
+
 let signupdata = document.querySelector("#signupForm");
 let logindata = document.querySelector("#loginForm");
 
@@ -12,28 +13,20 @@ logbtn.addEventListener("click", function(event){
     event.preventDefault();
     logindata.style.display="flex";
     signupdata.style.display="none";
-
-    
-
 });
 
 
-// let regbtn= document.querySelector("#registerbtn");
-// regbtn.addEventListener("click", function(event){
-//     signupForm.style.display="flex";
+let loginbtn=document.querySelector(".loginbtn");
+loginbtn.addEventListener("click", function(event){
+    event.preventDefault();
+    // alert("Login Successful");
+   
+    window.location.href = "./home.html"; 
+});
 
 
-// });
+// Initially show the login form and hide the signup form
 
-// function showSignUp(){
-//     let signupForm = document.getElementById("signupForm");
-//     let loginForm = document.getElementById("loginForm");
-//     signupForm.style.display="flex";
-//     loginForm.style.display="none";
 
-// }
 
-// let name = document.getElementById("name").value;
-// let email = document.getElementById("email").value;
-// let password = document.getElementById("password").value;   
 
